4eddcef0ea4c7420907ac630679e82d4ee27c72c
lauragallo <gallolaura22@gmail.com>
2024-04-22 18:53:21 +0200

    Stampa di prova GUI

--- src/Minesweeper.java
+++ src/Minesweeper.java
@@ -1,5 +1,10 @@
 public class Minesweeper {
-    public static void main(String[] args){
-        System.out.println(new Board(10,10));
+    public static void main(String[] args) {
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                ProvaGUI.createAndShowGUI();
+            }
+        });
     }
+
 }
